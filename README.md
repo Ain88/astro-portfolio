fa77e6ed7a5fe2c46eabda164235f6fcf8f89fc8
ain88 <ahyoung.in@hotmail.com>
2023-09-10 15:29:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Ahyoung In - Portfolio
+
+Welcome to my portfolio website's repository. This website serves as a showcase of my skills, experience, and featured projects.
+
+## About Me
+
+I'm Ahyoung In, a programmer with a background in Political Science and Business. My passion lies in programming, with a focus on Asp.net Core MVC, React, and Web Design (WordPress, Shopify). I thrive on problem-solving and am always eager to learn, including exploring no-code web design tools.
+
+## Experience
+
+### AMPM Service
+- **Aug 2021 - Present**
+- Port Coquitlam, BC, Canada
+
+### Web Developer
+- **Oct 2020 - Aug 2021**
+- West Vancouver, BC, Canada
+
+### Blue Humming Therapy
+- **Sep 2019 - Nov 2019**
+- San Francisco, CA, USA
+
+### Samsung Techwin
+- **Jan 2013 - Aug 2013**
+- Pangyo, South Korea
+
+## Education
+
+- **Openclassrooms**
+  - Full Stack Software Developer Bootcamp Course
+
+- **Okanagan College**
+  - Computer Information Systems Diploma
+
+- **Incheon National University**
+  - Bachelor's Degree in Political Science and International Trade
+
+## Featured Project
+
+**AMPM Mobile Website (2023)**
+
+Feel free to explore my website to learn more about my projects and skills.
---
+++ src/data/info.ts
@@ -0,0 +1,116 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Ahyoung In",
+  jobDescription: "Web Developer",
+  about: `After earning a degree in Political Science and Business, I switched gears to follow my passion for programming. 
+  I completed a Computer Science Diploma, specializing in Computer Programming, with a focus on Asp.net Core MVC, React, and Web Design, 
+  including WordPress and Shopify. Problem-solving is my forte, and I thrive on cracking complex challenges.
+  `,
+  about2: `Apart from coding, I'm an avid badminton player and a culinary enthusiast. 
+  I'm constantly expanding my horizons by diving into new technologies, currently exploring no-code web design tools.`
+  ,
+
+  experience: [
+    {
+      name: "AMPM Service",
+      location: "Port Coquitlam, BC, Canada",
+      startDate: "Aug 2021",
+      endDate: "Present",
+      description: [
+        "- Implemented new features and fixed bugs on web applications",
+        "- Created new landing pages and maintained websites using Html, Css, Javascript, Bootstrap, Microsoft SQL, Jquery, Asp.Net MVC",
+      ],
+    },
+
+    {
+      name: "Web Developer",
+      location: "West Vancouver, BC, Canada",
+      startDate: "Oct 2020",
+      endDate: "Aug 2021",
+      description: [
+        "- Debugged and fixed existing website/application for continuous improvement",
+        "- Implemented new plugins and updates to Magento/Woocommerce website",
+        "- Developed new websites using LAMP stack, hosted websites using Mysql/Php for backend, CSS/Sass/Html template/Javascript/Bootstrap/Jquery for frontend",
+      ],
+    },
+
+    {
+      name: "Blue Humming Therapy",
+      location: "San Francisco, CA, USA",
+      startDate: "Sep 2019",
+      endDate: "Nov 2019",
+      description: [
+        "- Re-designed the existing Wix website to have a consistent, modern look",
+        "- Followed direction of the manager regarding the idea of the website",
+      ],
+    },
+
+    {
+      name: "Samsung Techwin",
+      location: "Pangyo, South Korea",
+      startDate: "Jan 2013",
+      endDate: "Aug 2013",
+      description: [
+        "- Checked the quality and accuracy of the document, tracked revision of the document",
+        "- Coordinated document exchanges between departments, contractors, suppliers, and customers",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Openclassrooms",
+      location: "Remote",
+      startDate: "2020",
+      endDate: "2021",
+      description: [
+        "Full Stack Software Developer Bootcamp Course",
+      ],
+    },
+    {
+      name: "Okanagan College",
+      location: "Kelowna, BC, Canada",
+      startDate: "2014",
+      endDate: "2016",
+      description: [
+        "Computer Information Systems Diploma",
+      ]
+    },
+    {
+      name: "Incheon National University",
+      location: "Incheon, South Korea",
+      startDate: "2007",
+      endDate: "2013",
+      description: [
+        "Political Science Bachelor’s Degree and International Trade Bachelor’s Degree",
+      ]
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/ain88",
+    email: "ahyoung.ingmail.com",
+    linkedin: "https://www.linkedin.com/in/ahyoung-in",
+  },
+
+  projects: [
+    {
+      title: "AMPM Mobile Website",
+      isFeatured: true,
+      thumbnail: "/assets/images/ampm-mobile.webp",
+      liveUrl: "https://ampmmobile.com",
+    },
+    {
+      title: "Immigration Company Website",
+      isFeatured: false,
+      thumbnail: "/assets/images/petercan.webp",
+      liveUrl: "https://petercan.ca",
+    },
+    {
+      title: "User Manual Web Application",
+      isFeatured: false,
+      thumbnail: "/assets/images/sddocs.webp",
+      liveUrl: "https://docs.sdcloudpos.com",
+    },
+  ],
+};
